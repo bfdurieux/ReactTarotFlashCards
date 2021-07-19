beafe6250231aeaf155177c73a98f00ab6859643
Bruno Durieux <b.durieux@cppro.com.br>
2021-07-19 20:09:24 -0300

    Updates

--- src/Interfaces/IViewportSize.tsx
+++
@@ -1,4 +0,0 @@
-export interface IViewportSize {
-  width: number;
-  height: number;
-}
--- src/Spreads/CelticCrossSpread/CelticCrossSpread.tsx
+++
@@ -1,6 +0,0 @@
-import React, { Component } from "react";
-
-class CelticCrossSpread extends Component{
-
-}
-export default CelticCrossSpread;
